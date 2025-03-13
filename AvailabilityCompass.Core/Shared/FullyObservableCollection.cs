0400ac43b7e24f3658825b4d3eefffa8ea8cc892
sqisiii <sqisiii@gmail.com>
2025-03-13 01:56:12 +0100

    feat: Add FullyObservableCollection for enhanced collection change notifications. It reacts when property value changes.

---
+++ AvailabilityCompass.Core/Shared/FullyObservableCollection.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+
+namespace AvailabilityCompass.Core.Shared;
+
+public sealed class FullyObservableCollection<T> : ObservableCollection<T>
+    where T : INotifyPropertyChanged
+{
+    private readonly SynchronizationContext _syncContext;
+
+    public FullyObservableCollection()
+    {
+        _syncContext = SynchronizationContext.Current ?? new SynchronizationContext();
+        CollectionChanged += FullObservableCollectionCollectionChanged;
+    }
+
+    public FullyObservableCollection(IEnumerable<T> items) : this()
+    {
+        foreach (var item in items)
+        {
+            Add(item);
+        }
+    }
+
+    private void FullObservableCollectionCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+    {
+        if (e.NewItems != null)
+        {
+            foreach (var item in e.NewItems)
+            {
+                ((INotifyPropertyChanged)item).PropertyChanged += ItemPropertyChanged;
+            }
+        }
+
+        if (e.OldItems != null)
+        {
+            foreach (var item in e.OldItems)
+            {
+                ((INotifyPropertyChanged)item).PropertyChanged -= ItemPropertyChanged;
+            }
+        }
+    }
+
+    private void ItemPropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if (sender == null)
+            return;
+
+        var args = new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Replace, sender, sender, IndexOf((T)sender));
+        if (args.NewStartingIndex == -1 || args.OldStartingIndex == -1)
+            return;
+
+        if (SynchronizationContext.Current == _syncContext)
+        {
+            OnCollectionChanged(args);
+        }
+        else
+        {
+            _syncContext.Post(_ => OnCollectionChanged(args), null);
+        }
+    }
+}
