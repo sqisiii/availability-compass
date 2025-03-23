8d8e9193ab68da86ddf3e7d77005b4667229f01e
sqisiii <sqisiii@gmail.com>
2025-03-24 00:02:11 +0100

    feat: add SourceAdditionalDataName class for managing source additional data key names

---
+++ AvailabilityCompass.Core/Features/ManageSources/Sources/SourceAdditionalDataNames.cs
@@ -0,0 +1,13 @@
+﻿namespace AvailabilityCompass.Core.Features.ManageSources.Sources;
+
+public static class SourceAdditionalDataName
+{
+    public const string Type = "Type";
+    public const string Destination = "Destination";
+    public const string Code = "Code";
+    public const string Remarks = "Remarks";
+    public const string Price = "Price";
+    public const string Difficulty = "Difficulty";
+    public const string Status = "Status";
+    public const string IsNew = "Is New?";
+}
