f1704e6c046be2dacf22becca444b6faf5436be1
sqisiii <sqisiii@gmail.com>
2025-03-24 12:33:46 +0100

    docs: add XML documentation for IDbConnectionFactory and SqliteDbConnectionFactory

--- AvailabilityCompass.Core/Shared/Database/IDbConnectionFactory.cs
+++ AvailabilityCompass.Core/Shared/Database/IDbConnectionFactory.cs
@@ -2,7 +2,14 @@
 
 namespace AvailabilityCompass.Core.Shared.Database;
 
+/// <summary>
+/// Represents a factory for creating database connections.
+/// </summary>
 public interface IDbConnectionFactory
 {
+    /// <summary>
+    /// Creates and returns a new instance of a database connection.
+    /// </summary>
+    /// <returns>An instance of <see cref="IDbConnection"/>.</returns>
     IDbConnection Connect();
 }
--- AvailabilityCompass.Core/Shared/Database/SqliteDbConnectionFactory.cs
+++ AvailabilityCompass.Core/Shared/Database/SqliteDbConnectionFactory.cs
@@ -3,15 +3,26 @@ using System.Data.SQLite;
 
 namespace AvailabilityCompass.Core.Shared.Database;
 
+/// <summary>
+/// Factory class for creating SQLite database connections.
+/// </summary>
 public class SqliteDbConnectionFactory : IDbConnectionFactory
 {
     private readonly string _connectionString;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SqliteDbConnectionFactory"/> class with the specified connection string.
+    /// </summary>
+    /// <param name="connectionString">The connection string to use for the SQLite database.</param>
     public SqliteDbConnectionFactory(string connectionString)
     {
         _connectionString = connectionString;
     }
 
+    /// <summary>
+    /// Creates and returns a new instance of an SQLite database connection.
+    /// </summary>
+    /// <returns>An instance of <see cref="IDbConnection"/> representing the SQLite database connection.</returns>
     public IDbConnection Connect()
     {
         return new SQLiteConnection(_connectionString);
