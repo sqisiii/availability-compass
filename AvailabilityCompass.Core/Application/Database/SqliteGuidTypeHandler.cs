da9f82c7c5325c395eaf15a5398bdb90ca2c8dbd
sqisiii <sqisiii@gmail.com>
2025-03-24 03:04:07 +0100

    feat: add summary documentation for SqliteGuidTypeHandler

--- AvailabilityCompass.Core/Application/Database/SqliteGuidTypeHandler.cs
+++ AvailabilityCompass.Core/Application/Database/SqliteGuidTypeHandler.cs
@@ -3,6 +3,10 @@ using Dapper;
 
 namespace AvailabilityCompass.Core.Application.Database;
 
+/// <summary>
+/// Type handler for SQLite to support storing and retrieving GUIDs as binary data.
+/// This class enables Dapper to correctly map between .NET's Guid type and SQLite's BLOB storage.
+/// </summary>
 internal class SqliteGuidTypeHandler : SqlMapper.TypeHandler<Guid>
 {
     public override void SetValue(IDbDataParameter parameter, Guid value)
