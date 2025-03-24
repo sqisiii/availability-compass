0687312fcea18901539e71f2a8ef7995ff1a1307
sqisiii <sqisiii@gmail.com>
2025-03-24 12:39:28 +0100

    docs: add XML documentation for INavigationService and INavigationStore interfaces

--- AvailabilityCompass.Core/Shared/Navigation/INavigationService.cs
+++ AvailabilityCompass.Core/Shared/Navigation/INavigationService.cs
@@ -1,8 +1,19 @@
 namespace AvailabilityCompass.Core.Shared.Navigation;
 
+/// <summary>
+/// Defines a service for navigating between views and dialogs.
+/// </summary>
+/// <typeparam name="T">The type of the view model.</typeparam>
 public interface INavigationService<in T>
 {
+    /// <summary>
+    /// Navigates to the specified view model.
+    /// </summary>
+    /// <param name="viewModel">The view model to navigate to.</param>
     void NavigateTo(T viewModel);
 
+    /// <summary>
+    /// Closes the current view.
+    /// </summary>
     void CloseView();
 }
--- AvailabilityCompass.Core/Shared/Navigation/INavigationStore.cs
+++ AvailabilityCompass.Core/Shared/Navigation/INavigationStore.cs
@@ -1,7 +1,18 @@
 ﻿namespace AvailabilityCompass.Core.Shared.Navigation;
 
+/// <summary>
+/// Interface for a navigation store that holds the current view model.
+/// </summary>
+/// <typeparam name="T">The type of the view model.</typeparam>
 public interface INavigationStore<T>
 {
+    /// <summary>
+    /// Gets or sets the current view model.
+    /// </summary>
     T? CurrentViewModel { get; set; }
+
+    /// <summary>
+    /// Event that is triggered when the current view model changes.
+    /// </summary>
     event Action? CurrentViewModelChanged;
 }
