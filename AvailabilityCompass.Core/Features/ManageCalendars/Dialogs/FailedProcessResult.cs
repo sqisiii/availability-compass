8cbf9cfe4802884261756355d6b118272d794ed0
sqisiii <sqisiii@gmail.com>
2025-03-24 03:18:20 +0100

    feat: add summary documentation for FailedProcessResult class

--- AvailabilityCompass.Core/Features/ManageCalendars/Dialogs/FailedProcessResult.cs
+++ AvailabilityCompass.Core/Features/ManageCalendars/Dialogs/FailedProcessResult.cs
@@ -1,5 +1,8 @@
 ﻿namespace AvailabilityCompass.Core.Features.ManageCalendars.Dialogs;
 
+/// <summary>
+/// Represents a failed dialog processing result. Implements the <see cref="IProcessResult"/> interface.
+/// </summary>
 public class FailedProcessResult : IProcessResult
 {
     public bool IsSuccess => false;
