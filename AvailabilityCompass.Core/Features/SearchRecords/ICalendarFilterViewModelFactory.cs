afd9db1134753745a629f4dd8e5ab77f59900cae
sqisiii <sqisiii@gmail.com>
2025-03-24 04:10:25 +0100

    docs: add XML documentation for ICalendarFilterViewModelFactory and its Create method

--- AvailabilityCompass.Core/Features/SearchRecords/ICalendarFilterViewModelFactory.cs
+++ AvailabilityCompass.Core/Features/SearchRecords/ICalendarFilterViewModelFactory.cs
@@ -2,7 +2,15 @@
 
 namespace AvailabilityCompass.Core.Features.SearchRecords;
 
+/// <summary>
+/// Factory interface for creating instances of <see cref="CalendarFilterViewModel"/>.
+/// </summary>
 public interface ICalendarFilterViewModelFactory
 {
+    /// <summary>
+    /// Creates a collection of <see cref="CalendarFilterViewModel"/> from a collection of <see cref="GetCalendarsForFilteringResponse.CalendarDto"/>.
+    /// </summary>
+    /// <param name="calendars">The collection of <see cref="GetCalendarsForFilteringResponse.CalendarDto"/> to convert.</param>
+    /// <returns>A collection of <see cref="CalendarFilterViewModel"/>.</returns>
     IEnumerable<CalendarFilterViewModel> Create(IEnumerable<GetCalendarsForFilteringResponse.CalendarDto> calendars);
 }
