5226b9480c017361b0b6748651979e3c903d7413
sqisiii <sqisiii@gmail.com>
2025-03-24 03:37:11 +0100

    feat: add XML documentation for factory interfaces in calendar management

--- AvailabilityCompass.Core/Features/ManageCalendars/ICalendarViewModelFactory.cs
+++ AvailabilityCompass.Core/Features/ManageCalendars/ICalendarViewModelFactory.cs
@@ -1,9 +1,28 @@
 ﻿namespace AvailabilityCompass.Core.Features.ManageCalendars;
 
+/// <summary>
+/// Factory interface for creating calendar-related view models.
+/// </summary>
 public interface ICalendarViewModelFactory
 {
+    /// <summary>
+    /// Creates a CalendarViewModel from a CalendarDto.
+    /// </summary>
+    /// <param name="calendarDto">The data transfer object containing calendar data.</param>
+    /// <returns>A CalendarViewModel instance.</returns>
     CalendarViewModel CreateCalendar(CalendarDto calendarDto);
+
+    /// <summary>
+    /// Creates a SingleDateViewModel from a SingleDateDto.
+    /// </summary>
+    /// <param name="singleDateDto">The data transfer object containing single date data.</param>
+    /// <returns>A SingleDateViewModel instance.</returns>
     SingleDateViewModel CreateSingleDate(SingleDateDto singleDateDto);
 
+    /// <summary>
+    /// Creates a RecurringDateViewModel from a RecurringDateDto.
+    /// </summary>
+    /// <param name="recurringDateDto">The data transfer object containing recurring date data.</param>
+    /// <returns>A RecurringDateViewModel instance.</returns>
     RecurringDateViewModel CreateRecurringDate(RecurringDateDto recurringDateDto);
 }
