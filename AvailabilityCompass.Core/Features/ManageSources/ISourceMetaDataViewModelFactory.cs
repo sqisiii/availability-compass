cf12cc66944a1e174be6583861b405a01c04a9d0
sqisiii <sqisiii@gmail.com>
2025-03-24 03:51:01 +0100

    docs: add XML documentation for ISourceMetaDataViewModelFactory and ISourceStore interfaces

--- AvailabilityCompass.Core/Features/ManageSources/ISourceMetaDataViewModelFactory.cs
+++ AvailabilityCompass.Core/Features/ManageSources/ISourceMetaDataViewModelFactory.cs
@@ -2,7 +2,15 @@ using AvailabilityCompass.Core.Features.ManageSources.Queries.GetSourcesMetaData
 
 namespace AvailabilityCompass.Core.Features.ManageSources;
 
+/// <summary>
+/// Factory interface for creating instances of <see cref="SourceMetaDataViewModel"/> from a collection of <see cref="GetSourcesMetaDataFromDbDto"/>.
+/// </summary>
 public interface ISourceMetaDataViewModelFactory
 {
+    /// <summary>
+    /// Creates a collection of <see cref="SourceMetaDataViewModel"/> from the provided collection of <see cref="GetSourcesMetaDataFromDbDto"/>.
+    /// </summary>
+    /// <param name="sourcesMetaData">The collection of source metadata DTOs.</param>
+    /// <returns>A collection of <see cref="SourceMetaDataViewModel"/>.</returns>
     IEnumerable<SourceMetaDataViewModel> Create(IEnumerable<GetSourcesMetaDataFromDbDto> sourcesMetaData);
 }
--- AvailabilityCompass.Core/Features/ManageSources/ISourceStore.cs
+++ AvailabilityCompass.Core/Features/ManageSources/ISourceStore.cs
@@ -1,6 +1,13 @@
 namespace AvailabilityCompass.Core.Features.ManageSources;
 
+/// <summary>
+/// Represents a store for managing source metadata.
+/// </summary>
 public interface ISourceStore
 {
+    /// <summary>
+    /// Retrieves a list of source metadata.
+    /// </summary>
+    /// <returns>A list of <see cref="SourceMetaData"/> objects.</returns>
     IList<SourceMetaData> GetSourceMetaData();
 }
