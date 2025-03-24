694079633b77864ab2a8ba7ca85c1dbc7f489fa4
sqisiii <sqisiii@gmail.com>
2025-03-24 12:44:04 +0100

    docs: add XML documentation for IAbstractFactory interface and its Create method

--- AvailabilityCompass.Core/Shared/IAbstractFactory.cs
+++ AvailabilityCompass.Core/Shared/IAbstractFactory.cs
@@ -1,6 +1,14 @@
 ﻿namespace AvailabilityCompass.Core.Shared;
 
+/// <summary>
+/// Defines a generic interface for an abstract factory that creates instances of type <typeparamref name="T"/>.
+/// </summary>
+/// <typeparam name="T">The type of object that the factory creates.</typeparam>
 public interface IAbstractFactory<out T>
 {
+    /// <summary>
+    /// Creates an instance of type <typeparamref name="T"/>.
+    /// </summary>
+    /// <returns>An instance of type <typeparamref name="T"/>.</returns>
     T Create();
 }
