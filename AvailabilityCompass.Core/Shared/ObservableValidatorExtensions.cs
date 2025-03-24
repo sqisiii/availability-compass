17401527846aa628de7f63198d7ff082a3d8af12
sqisiii <sqisiii@gmail.com>
2025-03-24 12:51:12 +0100

    docs: add XML documentation for ObservableValidatorExtensions class and ValidateAll method

--- AvailabilityCompass.Core/Shared/ObservableValidatorExtensions.cs
+++ AvailabilityCompass.Core/Shared/ObservableValidatorExtensions.cs
@@ -3,8 +3,18 @@ using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace AvailabilityCompass.Core.Shared;
 
+/// <summary>
+/// Provides extension methods for the <see cref="ObservableValidator"/> class.
+/// </summary>
 public static class ObservableValidatorExtensions
 {
+    /// <summary>
+    /// Validates all properties of the specified <see cref="ObservableValidator"/> instance.
+    /// </summary>
+    /// <remarks>
+    /// ObservableValidator defines ValidateAllProperties as protected so reflection is used to call it.
+    /// </remarks>
+    /// <param name="validator">The <see cref="ObservableValidator"/> instance to validate.</param>
     public static void ValidateAll(this ObservableValidator validator)
     {
         // Use reflection to call the protected method
