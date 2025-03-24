0d6dfad75abbcb763d11be0f56e9a84de3fd8767
sqisiii <sqisiii@gmail.com>
2025-03-24 12:37:31 +0100

    docs: add XML documentation for IEventBus interface and its methods

--- AvailabilityCompass.Core/Shared/EventBus/IEventBus.cs
+++ AvailabilityCompass.Core/Shared/EventBus/IEventBus.cs
@@ -1,8 +1,27 @@
 ﻿namespace AvailabilityCompass.Core.Shared.EventBus;
 
+/// <summary>
+/// Interface for an event bus that allows publishing and listening to events.
+/// </summary>
 public interface IEventBus
 {
+    /// <summary>
+    /// Publishes an event to the event bus.
+    /// </summary>
+    /// <typeparam name="TEvent">The type of the event.</typeparam>
+    /// <param name="evt">The event to publish.</param>
     void Publish<TEvent>(TEvent evt);
+
+    /// <summary>
+    /// Listens for events of a specific type.
+    /// </summary>
+    /// <typeparam name="TEvent">The type of the event to listen for.</typeparam>
+    /// <returns>An observable sequence of events of the specified type.</returns>
     IObservable<TEvent> Listen<TEvent>();
+
+    /// <summary>
+    /// Listens for all events.
+    /// </summary>
+    /// <returns>An observable sequence of all events.</returns>
     IObservable<object> ListenToAll();
 }
