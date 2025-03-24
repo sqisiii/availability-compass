2c98cd55021948e8c1ff82d38c190128fbd1697d
sqisiii <sqisiii@gmail.com>
2025-03-24 04:11:07 +0100

    docs: add XML documentation for ISourceFilterViewModelFactory and its Create method

--- AvailabilityCompass.Core/Features/SearchRecords/ISourceFilterViewModelFactory.cs
+++ AvailabilityCompass.Core/Features/SearchRecords/ISourceFilterViewModelFactory.cs
@@ -2,7 +2,15 @@
 
 namespace AvailabilityCompass.Core.Features.SearchRecords;
 
+/// <summary>
+/// Factory interface for creating instances of <see cref="SourceFilterViewModel"/>.
+/// </summary>
 public interface ISourceFilterViewModelFactory
 {
+    /// <summary>
+    /// Creates a new instance of <see cref="SourceFilterViewModel"/> based on the provided source response.
+    /// </summary>
+    /// <param name="getSourcesResponse">The source response containing the data needed to create the view model.</param>
+    /// <returns>A new instance of <see cref="SourceFilterViewModel"/>.</returns>
     SourceFilterViewModel Create(GetSourcesForFilteringResponse.Source getSourcesResponse);
 }
