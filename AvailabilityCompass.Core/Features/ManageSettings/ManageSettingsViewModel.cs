066b5676059e2cd15c9ebd3871984ffb3a467a8e
sqisiii <sqisiii@gmail.com>
2025-03-24 03:42:09 +0100

    feat: add XML documentation for ManageSettingsViewModel and introduce IsActive property

--- AvailabilityCompass.Core/Features/ManageSettings/ManageSettingsViewModel.cs
+++ AvailabilityCompass.Core/Features/ManageSettings/ManageSettingsViewModel.cs
@@ -3,9 +3,12 @@ using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace AvailabilityCompass.Core.Features.ManageSettings;
 
-[ObservableRecipient]
+/// <summary>
+/// View model for managing application settings.
+/// </summary>
 public partial class ManageSettingsViewModel : ObservableValidator, IPageViewModel
 {
+    public bool IsActive { get; set; }
     public string Icon => "CogOutline";
 
     public string Name => "Settings";
