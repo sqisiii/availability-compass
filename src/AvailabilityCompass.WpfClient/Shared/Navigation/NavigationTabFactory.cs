4881e3f0eb61a3564b8ac4912d4e37a0dcd96523
sqisiii <sqisiii@gmail.com>
2026-01-18 21:31:30 +0100

    refactor: remove unused manage settings view and view model

--- src/AvailabilityCompass.WpfClient/Shared/Navigation/NavigationTabFactory.cs
+++ src/AvailabilityCompass.WpfClient/Shared/Navigation/NavigationTabFactory.cs
@@ -1,38 +1,37 @@
 ﻿using System.Collections.ObjectModel;
 using AvailabilityCompass.Core.Features.ManageCalendars;
-using AvailabilityCompass.Core.Features.ManageSettings;
 using AvailabilityCompass.Core.Features.ManageSources;
 using AvailabilityCompass.Core.Features.SearchRecords;
 
 namespace AvailabilityCompass.WpfClient.Shared.Navigation;
 
+/// <summary>
+/// Factory for creating navigation tabs from registered page view models.
+/// </summary>
 public class NavigationTabFactory : INavigationTabFactory
 {
     private readonly ManageCalendarsViewModel _manageCalendarsViewModel;
-    private readonly ManageSettingsViewModel _manageSettingsViewModel;
     private readonly ManageSourcesViewModel _manageSourcesViewModel;
     private readonly SearchViewModel _searchViewModel;
 
     public NavigationTabFactory(
         SearchViewModel searchViewModel,
         ManageCalendarsViewModel manageCalendarsViewModel,
-        ManageSettingsViewModel manageSettingsViewModel,
         ManageSourcesViewModel manageSourcesViewModel)
     {
         _manageCalendarsViewModel = manageCalendarsViewModel;
-        _manageSettingsViewModel = manageSettingsViewModel;
         _manageSourcesViewModel = manageSourcesViewModel;
         _searchViewModel = searchViewModel;
     }
 
+    /// <inheritdoc />
     public ObservableCollection<NavigationTabModel> CreateNavigationTabs()
     {
         return
         [
             new NavigationTabModel(_searchViewModel),
             new NavigationTabModel(_manageCalendarsViewModel),
-            new NavigationTabModel(_manageSourcesViewModel),
-            new NavigationTabModel(_manageSettingsViewModel)
+            new NavigationTabModel(_manageSourcesViewModel)
         ];
     }
 }
