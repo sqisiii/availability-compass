6f0f79594c3dc3afa9f2bc63b31f710cf95a062e
sqisiii <sqisiii@gmail.com>
2026-02-06 02:34:21 +0100

    fix: fix tutorial's targets resizing issue

--- src/Guidely.WPF/AttachedProperties/TutorialTarget.cs
+++ src/Guidely.WPF/AttachedProperties/TutorialTarget.cs
@@ -21,19 +21,19 @@ public static class TutorialTarget
     /// <summary>
     /// Gets the tutorial target element name for a dependency object.
     /// </summary>
-    public static string? GetElement(DependencyObject obj)
-        => (string?)obj.GetValue(ElementProperty);
+    public static string? GetElement(DependencyObject obj) => (string?)obj.GetValue(ElementProperty);
 
     /// <summary>
     /// Sets the tutorial target element name for a dependency object.
     /// </summary>
-    public static void SetElement(DependencyObject obj, string? value)
-        => obj.SetValue(ElementProperty, value);
+    public static void SetElement(DependencyObject obj, string? value) => obj.SetValue(ElementProperty, value);
 
     private static void OnElementChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
     {
         if (d is not FrameworkElement element)
+        {
             return;
+        }
 
         var oldValue = e.OldValue as string;
         var newValue = e.NewValue as string;
@@ -43,6 +43,7 @@ public static class TutorialTarget
         {
             element.Loaded -= OnElementLoaded;
             element.Unloaded -= OnElementUnloaded;
+            element.SizeChanged -= OnElementSizeChanged;
             TutorialElementRegistry.Unregister(oldValue, element);
         }
 
@@ -56,33 +57,61 @@ public static class TutorialTarget
         element.Unloaded += OnElementUnloaded;
 
         // If already loaded, register immediately
-        if (element.IsLoaded)
+        if (!element.IsLoaded)
         {
-            TutorialElementRegistry.Register(newValue, element);
+            return;
         }
+
+        TutorialElementRegistry.Register(newValue, element);
+        element.SizeChanged += OnElementSizeChanged;
     }
 
     private static void OnElementLoaded(object sender, RoutedEventArgs e)
     {
         if (sender is not FrameworkElement element)
+        {
             return;
+        }
 
         var targetName = GetElement(element);
-        if (!string.IsNullOrEmpty(targetName))
+        if (string.IsNullOrEmpty(targetName))
         {
-            TutorialElementRegistry.Register(targetName, element);
+            return;
         }
+
+        TutorialElementRegistry.Register(targetName, element);
+        element.SizeChanged += OnElementSizeChanged;
     }
 
     private static void OnElementUnloaded(object sender, RoutedEventArgs e)
     {
         if (sender is not FrameworkElement element)
+        {
             return;
+        }
 
         var targetName = GetElement(element);
-        if (!string.IsNullOrEmpty(targetName))
+        if (string.IsNullOrEmpty(targetName))
         {
-            TutorialElementRegistry.Unregister(targetName, element);
+            return;
+        }
+
+        element.SizeChanged -= OnElementSizeChanged;
+        TutorialElementRegistry.Unregister(targetName, element);
+    }
+
+    private static void OnElementSizeChanged(object sender, SizeChangedEventArgs e)
+    {
+        if (sender is not FrameworkElement element)
+        {
+            return;
+        }
+
+        var targetName = GetElement(element);
+        if (!string.IsNullOrEmpty(targetName) && element.IsLoaded)
+        {
+            // Re-register to fire ElementChanged and refresh cutouts/highlights
+            TutorialElementRegistry.Register(targetName, element);
         }
     }
 }
