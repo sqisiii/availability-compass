464e0040fc15ba54b55525c327bf246f64fc3624
sqisiii <sqisiii@gmail.com>
2026-02-11 20:34:53 +0100

    build: use Guidely nuget packages instead of local code.

--- src/Guidely.Core/Abstractions/TutorialContextBase.cs
+++
@@ -1,7 +0,0 @@
-namespace Guidely.Core.Abstractions;
-
-/// <summary>
-/// Base class for tutorial context records.
-/// Consumers should create a record inheriting from this class.
-/// </summary>
-public abstract record TutorialContextBase;
